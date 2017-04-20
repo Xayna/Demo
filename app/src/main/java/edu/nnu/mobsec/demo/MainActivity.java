9ce286fa571a8ecdd65dae5fb9bd83f677893eba
zainamalhis <zaina.malhis@icecube-tech.com>
2017-04-20 22:12:54 +0300

    Initial commit

---
+++ app/src/main/java/edu/nnu/mobsec/demo/MainActivity.java
@@ -0,0 +1,85 @@
+package edu.nnu.mobsec.demo;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if(canToggleGPS())
+        {
+            Toast.makeText(getBaseContext() , "GPS is toggable" , Toast.LENGTH_LONG).show();
+            Log.i("Toggle" , "woohaaa !!");
+           // turnGPSOff();
+            turnGPSOn();
+        }
+        else
+        {
+            Log.i("Toggle" , "Can't change gps");
+
+        }
+
+    }
+
+    private boolean canToggleGPS() {
+        PackageManager pacman = getPackageManager();
+        PackageInfo pacInfo = null;
+
+        try {
+            pacInfo = pacman.getPackageInfo("com.android.settings", PackageManager.GET_RECEIVERS);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            return false; //package not found
+        }
+
+        if(pacInfo != null){
+            for(ActivityInfo actInfo : pacInfo.receivers){
+                //test if recevier is exported. if so, we can toggle GPS.
+                Log.i("Toggle" , "pacInfo not null");
+                if(actInfo.name.equals("com.android.settings.widget.SettingsAppWidgetProvider") && actInfo.exported){
+                    Log.i("Toggle" , "com.android.settings.widget.SettingsAppWidgetProvider");
+                    return true;
+                }
+            }
+        }
+
+        return false; //default
+    }
+
+    private void turnGPSOn(){
+        String provider = Settings.Secure.getString(getContentResolver(), Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
+
+        if(!provider.contains("gps")){ //if gps is disabled
+            final Intent poke = new Intent();
+            poke.setClassName("com.android.settings", "com.android.settings.widget.SettingsAppWidgetProvider");
+            poke.addCategory(Intent.CATEGORY_ALTERNATIVE);
+            poke.setData(Uri.parse("3"));
+            sendBroadcast(poke);
+            Toast.makeText(getBaseContext() , "GPs is on" , Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void turnGPSOff() {
+        String provider = Settings.Secure.getString(getContentResolver(), Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
+
+        if (provider.contains("gps")) { //if gps is enabled
+            final Intent poke = new Intent();
+            poke.setClassName("com.android.settings", "com.android.settings.widget.SettingsAppWidgetProvider");
+            poke.addCategory(Intent.CATEGORY_ALTERNATIVE);
+            poke.setData(Uri.parse("3"));
+            sendBroadcast(poke);
+        }
+    }
+}
