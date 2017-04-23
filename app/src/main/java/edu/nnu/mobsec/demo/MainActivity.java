7a42359de1f4cd1ad4bf5d6997690c47d289bc9a
zainamalhis <zaina.malhis@icecube-tech.com>
2017-04-23 12:17:58 +0300

    Adding comments.

--- app/src/main/java/edu/nnu/mobsec/demo/MainActivity.java
+++ app/src/main/java/edu/nnu/mobsec/demo/MainActivity.java
@@ -28,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         else
         {
             Log.i("Toggle" , "Can't change gps");
+            //this is a test comment
 
         }
 
